97c98af1da0e7222a6ff1c3250a614a1a0031f79
Boaz Crezee <bjcrezee@gmail.com>
2024-09-10 22:05:09 +0200

    added some classes and structure (it created some gaps) to the dashboard.htm table and finished the javascript theoretically

--- scripts/study_progress_meter.js
+++ scripts/study_progress_meter.js
@@ -1,33 +1,59 @@
 window.addEventListener("DOMContentLoaded", init(), false);
 
-function init(){
+function init() {
     setProgressMeter();
 }
 
 function setProgressMeter() {
-    const courseElements = document.querySelectorAll(".course");
-    for(const courseElement of courseElements){
-        checkCoursePassed(courseElement);
-    }
     const progressMeter = document.getElementById("progressMeter");
-    progressMeter.value = 5;
+    progressMeter.value = getTotalCreditsEarned();
+}
+
+function getTotalCreditsEarned() {
+    let totalCreditsEarned = 0;
+    const courseElements = document.querySelectorAll(".course");
+    if(courseElements === null) return totalCreditsEarned;
+    for (const courseElement of courseElements) {
+        totalCreditsEarned += getCourseCreditsEarned(courseElement);
+    }
+    return totalCreditsEarned;
+}
+
+function getCourseCreditsEarned(courseElement) {
+    const isCoursePassed = checkCoursePassed(courseElement);
+    const credits = getFloatFromQuerySelector(courseElement, ".credits");
+    const creditsEarned = isCoursePassed ? credits : 0;
+    return creditsEarned;
 }
 
 function checkCoursePassed(courseElement) {
-    const gradeElements = courseElement.querySelectorAll(".grade");
-    let averageGrade = new weightedValue(0, 0);
-    for (const gradeElement of gradeElements) {
-        const newGrade = loadNewGrade(gradeElement);
-        averageGrade = addToWeightedAverage(newGrade, averageGrade);
-    }
-    const isCoursePassed = averageGrade.value >= 5.5;
-    return isCoursePassed;
+    return getCourseGrade(courseElement) >= 5.5;
 }
 
-function loadNewGrade(gradeElement) {
-    const grade  = parseFloat(gradeElement.getElementById("grade").innerHTML);
-    const weight = parseFloat(gradeElement.getElementById("weight").innerHTML);
+function getCourseGrade(courseElement) {
+    const examElements = courseElement.querySelectorAll(".exam");
+    let averageGrade = new weightedValue(0, 0);
+    if (examElements === null) return averageGrade.value;
+    for (const examElement of examElements) {
+        const newGrade = loadNewGrade(examElement);
+        averageGrade = addToWeightedAverage(newGrade, averageGrade);
+    }
+    return averageGrade.value;
+}
+
+function loadNewGrade(examElement) {
+    const grade  = getFloatFromQuerySelector(examElement, ".grade");
+    const weight = getFloatFromQuerySelector(examElement, ".weight");
     return new weightedValue(grade, weight);
+}
+
+function getFloatFromQuerySelector(parent, query) {
+    if(parent===null || parent===undefined) return null;
+    const element = parent.querySelector(query);
+    if(element===null) return null;
+    const number = parseFloat( element.innerHTML );
+    if(number===NaN) return null;
+    return number;
 }
 
 function addToWeightedAverage(newValue, averageValue) {
